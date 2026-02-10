fff8b4deb6cc4f6b7050dcedcfd6d24ce817cf38
pphatdev <pphat.kits@gmail.com>
2026-02-10 12:54:07 +0700

    feat: update folder card component and my-file view for improved styling and structure

--- src/client/utils/modules.ts
+++ src/client/utils/modules.ts
@@ -16,13 +16,13 @@ export const modules = [
         className: defaultClass,
         activeClassName: activeClass
     },
-    {
-        name: "Shared",
-        url: "/shared",
-        icon: `<svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path></svg>`,
-        className: defaultClass,
-        activeClassName: activeClass
-    },
+    // {
+    //     name: "Shared",
+    //     url: "/shared",
+    //     icon: `<svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path></svg>`,
+    //     className: defaultClass,
+    //     activeClassName: activeClass
+    // },
     {
         name: "Recent",
         url: "/recent",
