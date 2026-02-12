ccd18d3e35d12ea586790a77afaa6460e1231fb1
pphatdev <leatsophat02@gmail.com>
2026-02-12 22:49:47 +0700

    ✨ feat: add active URL handling for sidebar modules to improve navigation

--- src/client/utils/modules.ts
+++ src/client/utils/modules.ts
@@ -12,6 +12,7 @@ export const modules = [
     {
         name: "My Files",
         url: "/files",
+        activeUrls: ["/files", "/detail"],
         icon: `<svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"></path></svg>`,
         className: defaultClass,
         activeClassName: activeClass
@@ -23,31 +24,10 @@ export const modules = [
         className: defaultClass,
         activeClassName: activeClass
     },
-    // {
-    //     name: "Shared",
-    //     url: "/shared",
-    //     icon: `<svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path></svg>`,
-    //     className: defaultClass,
-    //     activeClassName: activeClass
-    // },
     {
         name: "Recent",
         url: "/recent",
         icon: `<svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>`,
-        className: defaultClass,
-        activeClassName: activeClass
-    },
-    {
-        name: "Starred",
-        url: "/starred",
-        icon: `<svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.175 0l-3.976 2.888c-.783.57-1.838-.197-1.539-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.382-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"></path></svg>`,
-        className: defaultClass,
-        activeClassName: activeClass
-    },
-    {
-        name: "Trash",
-        url: "/trash",
-        icon: `<svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg>`,
         className: defaultClass,
         activeClassName: activeClass
     }
